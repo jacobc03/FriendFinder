9bb9c4192c1750bbb93d9e5945827d34814cd797
Jacob <jacob@Jacobs-MacBook-Air.local>
2017-01-22 21:17:46 -0500

    app works/needs more styling

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,83 +1,28 @@
-var parkArray=[
+var parks=[
   {
     "name": "Biscayne National Park",
     "photo": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTDxbkVvfXLXofVrSxpGQB7totPyMcA41tWNfdynmVcpuQ7yJIS",
-    "scores": [
-      "1",
-      "1",
-      "1",
-      "1",
-      "1",
-      "1",
-      "1",
-      "1",
-      "1",
-      "1"
-    ]
+    "score": '10'
   },
   {
     "name": "Haleakala National Park",
     "photo": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ1yR1LP_qRTN2veby4NbphRWcQ-blbZIuZG9GN1OMZolcpXvW2",
-    "scores": [
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2",
-      "2"
-    ]
+    "score": '20'
   },
   {
     "name": "Joshua Tree National Park",
     "photo": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQx8uoivVksZcWosyLCT2D06uKXMe-S8JGNQADliQJF5AjW1OvAWg",
-    "scores": [
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3",
-      "3"
-    ]
+    "score": '30'
   },
   {
     "name": "Yosemite National Park",
     "photo": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTLO2d-RX_PF4BiaNf6ZNnT1oB6dkE_wyNod17voHtgmrIh6PoXww",
-    "scores": [
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4",
-      "4"
-    ]
+    "score": '40'
   },
   {
     "name": "Grand Teton National Park",
     "photo": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQG95ePlMKPygbslSisnYj5DIsFi8448EIMOlhljV8WmVJ8QqK2",
-    "scores": [
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5",
-      "5"
-    ]
+    "score": '50'
   }
-];
-module.exports= parkArray;
+]
+module.exports= parks;
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,8 +1,34 @@
-var path =require('path');
-var parkData = require ('../data/friends.js');
+var path = require('path');
+var parks = require('./../data/friends.js');
+
+Array.prototype.min = function() {
+	return Math.min.apply(null, this);
+};
+
+var parkScores = [];
+for (var i=0; i<parks.length; i++) {
+	parkScores.push(parks[i].score);
+}
 
 module.exports = function(app){
-  app.post('/api/friends', function(req, res) {
-    res.json(parkData);
-  });
+    app.post('/api/friends', function(req, res) {
+    	var matchscores = [];
+    	var matches = [];
+    	var userscore = eval(req.body.scores.toString().replace(/,/g,'+'));
+    	for (var i=0; i<parkScores.length; i++) {
+    		matchscores[i] = Math.abs(userscore-parkScores[i]);
+    	}
+    	for (var i=0; i<matchscores.length; i++) {
+    		if (matchscores.min() == matchscores[i]) {
+    			matches.push(parks[i]);
+    		}
+    	}
+    	if (matches.length = 1) {
+    		console.log(matches);
+    		res.json(matches[0]);
+    	}
+    	else {
+    		res.json(matches[Math.floor(matches.length * Math.random())]);
+    	}
+	});
 }
