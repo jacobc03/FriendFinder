3cd5fd8c4020454cd7f9318f27a3621bada3b77f
Jacob <jacob@Jacobs-MacBook-Air.local>
2017-01-22 23:44:32 -0500

    added descriptions

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,27 +2,32 @@ var parks=[
   {
     "name": "Biscayne National Park",
     "photo": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTDxbkVvfXLXofVrSxpGQB7totPyMcA41tWNfdynmVcpuQ7yJIS",
-    "score": '10'
+    "score": '10',
+    "description":"Within sight of downtown Miami, yet worlds away, Biscayne protects a rare combination of aquamarine waters, emerald islands, and fish-bejeweled coral reefs. Here too is evidence of 10,000 years of human history, from pirates and shipwrecks to pineapple farmers and presidents. Outdoors enthusiasts can boat, snorkel, camp, watch wildlife…or simply relax in a rocking chair gazing out over the bay."
   },
   {
     "name": "Haleakala National Park",
     "photo": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcQ1yR1LP_qRTN2veby4NbphRWcQ-blbZIuZG9GN1OMZolcpXvW2",
-    "score": '20'
+    "score": '20',
+    "description":"This special place vibrates with stories of ancient and modern Hawaiian culture and protects the bond between the land and its people. The park also cares for endangered species, some of which exist nowhere else. Come visit this special place - renew your spirit amid stark volcanic landscapes and sub-tropical rain forest with an unforgettable hike through the backcountry."
   },
   {
     "name": "Joshua Tree National Park",
     "photo": "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQx8uoivVksZcWosyLCT2D06uKXMe-S8JGNQADliQJF5AjW1OvAWg",
-    "score": '30'
+    "score": '30',
+    "description":"Two distinct desert ecosystems, the Mojave and the Colorado, come together in Joshua Tree National Park. A fascinating variety of plants and animals make their homes in a land sculpted by strong winds and occasional torrents of rain. Dark night skies, a rich cultural history, and surreal geologic features add to the wonder of this vast wilderness in southern California."
   },
   {
     "name": "Yosemite National Park",
     "photo": "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTLO2d-RX_PF4BiaNf6ZNnT1oB6dkE_wyNod17voHtgmrIh6PoXww",
-    "score": '40'
+    "score": '40',
+    "description":"Not just a great valley, but a shrine to human foresight, the strength of granite, the power of glaciers, the persistence of life, and the tranquility of the High Sierra."
   },
   {
     "name": "Grand Teton National Park",
     "photo": "https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQG95ePlMKPygbslSisnYj5DIsFi8448EIMOlhljV8WmVJ8QqK2",
-    "score": '50'
+    "score": '50',
+    "description":"Rising above a scene rich with extraordinary wildlife, pristine lakes, and alpine terrain, the Teton Range stands monument to the people who fought to protect it. These are mountains of the imagination."
   }
 ]
 module.exports= parks;
